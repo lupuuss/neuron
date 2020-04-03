68806f4af38c1fd9fbd09ff32d0a5ab1c82e813a
lupuuss <damian.baczynski@gmail.com>
2020-04-03 22:56:32 +0200

    Adds ConsoleLoader.kt

---
+++ src/main/kotlin/ml/output/ConsoleLoader.kt
@@ -0,0 +1,31 @@
+package ml.output
+
+import kotlin.math.roundToInt
+
+class ConsoleLoader(private val characters: Int) {
+
+    private var progress = 0.0
+
+    fun update(value: Double) {
+        progress = value
+    }
+
+    private fun repeat(char: Char, times: Int): String = generateSequence { char }.take(times).joinToString(separator = "")
+
+    private fun fixedSizePercentage(n: Int): String {
+        val percentage = ((progress * 1000).roundToInt() / 10.0).toString()
+        val size = percentage.length
+
+        return repeat(' ', n - size) + percentage
+    }
+
+    fun print() {
+        val progress = (characters.toDouble() * progress).roundToInt()
+        val spaces = characters - progress
+        print("\r[${repeat('=', progress)}>${repeat(' ', spaces)}] ${fixedSizePercentage(5)}%")
+    }
+
+    fun close() {
+        println()
+    }
+}
